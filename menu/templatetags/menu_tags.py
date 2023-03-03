94ae5123ad29e539b90c6b29f97f7f322c9eef16
Roman Popov <52166414+rpopov94@users.noreply.github.com>
2023-03-03 10:39:49 +0300

    рекурсивный вывод меню

--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -5,6 +5,23 @@ from menu.models import MenuItem
 
 register = template.Library()
 
+def render_menu(menu_items, current_url):
+    menu_html = '<ul>'
+    for item in menu_items:
+        menu_html += '<li>'
+        if item.url and (current_url == item.url or current_url.startswith(item.url + '/')):
+            menu_html += '<a href="{}" class="active">{}</a>'.format(item.url, item.name)
+        else:
+            menu_html += '<a href="{}">{}</a>'.format(item.url, item.name)
+
+        if item.children.all():
+            menu_html += render_menu(item.children.all(), current_url)
+
+        menu_html += '</li>'
+    menu_html += '</ul>'
+
+    return mark_safe(menu_html)
+
 @register.simple_tag(takes_context=True)
 def draw_menu(context, menu_name):
     request = context['request']
@@ -15,28 +32,4 @@ def draw_menu(context, menu_name):
     if menu_items is None:
         menu_items = list(MenuItem.objects.filter(parent=None).prefetch_related('children'))
         cache.set(cache_key, menu_items)
-
-    menu_html = ''
-
-    for item in menu_items:
-        menu_html += '<li>'
-        if item.url and (current_url == item.url or current_url.startswith(item.url + '/')):
-            menu_html += '<a href="{}" class="active">{}</a>'.format(item.url, item.name)
-        else:
-            menu_html += '<a href="{}">{}</a>'.format(item.url, item.name)
-
-        if item.children.all():
-            menu_html += '<ul>'
-            for child in item.children.all():
-                menu_html += '<li>'
-                if child.url and (current_url == child.url or current_url.startswith(child.url + '/')):
-                    menu_html += '<a href="{}" class="active">{}</a>'.format(child.url, child.name)
-                else:
-                    menu_html += '<a href="{}">{}</a>'.format(child.url, child.name)
-                menu_html += '</li>'
-            menu_html += '</ul>'
-
-        menu_html += '</li>'
-
-    return mark_safe(menu_html)
-
+    return render_menu(menu_items, current_url)
